fff3f1f650381cb58a804f2450f0c1400cc61f9a
Carlos Valencia <valenciacarlos278@gmail.com>
2024-04-21 08:04:56 -0500

    [Refactor] Modify to use Postgres instead of MySql

--- modules/core/database/database.go
+++ modules/core/database/database.go
@@ -7,8 +7,9 @@ import (
 
 	accountDomain "github.com/carlosVal0/miniEconomyGoBackend/modules/account/domain/entities"
 	authDomain "github.com/carlosVal0/miniEconomyGoBackend/modules/authentication/domain/entities"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var dbBean *gorm.DB
@@ -18,9 +19,20 @@ func Connect() {
 	dbUser := os.Getenv("DB_USER")
 	dbPasswd := os.Getenv("DB_PASSWD")
 	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
-	connString := fmt.Sprintf("%v:%v@tcp(%v)/%v?parseTime=true", dbUser, dbPasswd, dbHost, dbName)
-	db, err := gorm.Open(mysql.Open(connString), &gorm.Config{})
+	dbSchema := os.Getenv("DB_SCHEMA")
+	dsn := fmt.Sprintf("user=%v password=%v dbname=%v port=%v host=%v TimeZone=America/Bogota", dbUser, dbPasswd, dbName, dbPort, dbHost)
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   dbSchema,
+			SingularTable: false,
+		},
+	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
